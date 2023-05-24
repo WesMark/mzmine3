07aa11b7af5c8e90ffd3c04612115780b9063275
m_wesn01 <wesner.mark@web.de>
2023-05-24 16:55:10 +0200

    Fixed bug with empty list.

--- src/main/java/io/github/mzmine/modules/dataprocessing/id_mean/MeanTask.java
+++ src/main/java/io/github/mzmine/modules/dataprocessing/id_mean/MeanTask.java
@@ -66,10 +66,12 @@ public class MeanTask extends AbstractTask {
   protected MeanTask(@NotNull Instant moduleCallDate, ParameterSet parameters) {
     super(null, moduleCallDate);
 
+    //Set parameters.
     featureLists = parameters.getValue(MeanParameters.featureList).getMatchingFeatureLists();
     numRows = Arrays.stream(featureLists).mapToInt(ModularFeatureList::getNumberOfRows).sum();
     exportEnabled = parameters.getValue(MeanParameters.export);
 
+    //Set export parameters.
     if (exportEnabled) {
       SimpleParameterSet exportParameter = parameters.getParameter(MeanParameters.export)
           .getEmbeddedParameters();
@@ -78,6 +80,7 @@ public class MeanTask extends AbstractTask {
       mzRange = Range.closed(mz - mzTolerance, mz + mzTolerance);
       filePath = exportParameter.getValue(MeanExportParameters.file).getAbsolutePath();
 
+      //Set export parameters to null/0 if export is disabled.
     } else {
       mz = 0;
       mzTolerance = 0;
@@ -101,29 +104,36 @@ public class MeanTask extends AbstractTask {
 
     setStatus(TaskStatus.PROCESSING);
 
+    //Creating an Excel workbook.
     XSSFWorkbook workbook = new XSSFWorkbook();
     XSSFSheet sheet = workbook.createSheet("RSD");
-    Row headerRow = sheet.createRow(0);
 
+    //Creating a header row in the Excel workbook.
+    Row headerRow = sheet.createRow(0);
     headerRow.createCell(0).setCellValue("File name");
     headerRow.createCell(1).setCellValue("feature m/z");
     headerRow.createCell(2).setCellValue("mean intensity");
     headerRow.createCell(3).setCellValue("rsd");
 
+    //Initializing decimal formats for the data to be exported.
     DecimalFormat mzFormat = new DecimalFormat("0.0000");
     DecimalFormat meanIntensityFormat = new DecimalFormat("0.00");
     DecimalFormat rsdFormat = new DecimalFormat("0.000");
 
+    //Going over all selected feature lists.
     for (int i = 0; i < featureLists.length; i++) {
 
       ModularFeatureList featureList = featureLists[i];
 
+      //Creating lists to add the data to be exported.
       List<Double> mzs = new ArrayList<>();
       List<Double> meanIntensities = new ArrayList<>();
       List<Double> rsds = new ArrayList<>();
 
+      //Going over all rows in the feature list.
       for (FeatureListRow row : featureList.getRows()) {
 
+        //Continuing with the next feature if the feature is null.
         Feature feature = row.getBestFeature();
         if (feature == null || feature.getFeatureStatus() == FeatureStatus.UNKNOWN) {
           processed++;
@@ -141,11 +151,11 @@ public class MeanTask extends AbstractTask {
         double meanIntensity = calculateMeanIntensity(intensities, numScans);
         row.set(MeanIntensityType.class, meanIntensity);
 
-        //RSD
+        //RSD.
         double rsd = calculateRSD(intensities, meanIntensity, numScans);
         row.set(RsdType.class, rsd);
 
-        //Adding the feature to the list for later export.
+        //Adding the feature to the list for later export if export is enabled.
         if (exportEnabled && mzRange.contains(feature.getMZ())) {
           mzs.add(Double.parseDouble(mzFormat.format(feature.getMZ())));
           meanIntensities.add(Double.parseDouble(meanIntensityFormat.format(meanIntensity)));
@@ -155,25 +165,30 @@ public class MeanTask extends AbstractTask {
         processed++;
       }
 
+      //Adding the data of the feature within the mz range with the closest mz value.
       if (exportEnabled) {
 
         int closestFeatureIndex = getClosestFeatureIndex(mzs);
 
         Row row = sheet.createRow(i + 1);
-
         row.createCell(0).setCellValue(featureList.getName());
-        row.createCell(1).setCellValue(mzs.get(closestFeatureIndex));
-        row.createCell(2).setCellValue(meanIntensities.get(closestFeatureIndex));
-        row.createCell(3).setCellValue(rsds.get(closestFeatureIndex));
+
+        if (closestFeatureIndex != -1) {
+          row.createCell(1).setCellValue(mzs.get(closestFeatureIndex));
+          row.createCell(2).setCellValue(meanIntensities.get(closestFeatureIndex));
+          row.createCell(3).setCellValue(rsds.get(closestFeatureIndex));
+        }
       }
     }
 
     if (exportEnabled) {
 
+      //Resizing the Excel sheets columns.
       for (int i = 0; i < sheet.getRow(0).getLastCellNum(); i++) {
         sheet.autoSizeColumn(i);
       }
 
+      //Writing the Excel file to disk.
       try {
         File file = new File(filePath + ".xlsx");
         FileOutputStream outputStream = new FileOutputStream(file);
@@ -188,34 +203,61 @@ public class MeanTask extends AbstractTask {
     setStatus(TaskStatus.FINISHED);
   }
 
-  private double calculateRSD(double[] intensities, double meanIntensity, int numScans) {
+  /**
+   * Calculates the relative standard deviation from an array of values.
+   *
+   * @param values
+   * @param meanValue
+   * @param numValues
+   * @return The relative standard deviation.
+   */
+  private double calculateRSD(double[] values, double meanValue, int numValues) {
 
     double sumDeviation = 0;
-    for (double intensity : intensities) {
+    for (double intensity : values) {
 
-      double deviation = Math.pow(intensity - meanIntensity, 2d);
+      double deviation = Math.pow(intensity - meanValue, 2d);
       sumDeviation += deviation;
     }
 
-    double sd = Math.sqrt(sumDeviation / numScans);
-    return sd / meanIntensity;
+    double sd = Math.sqrt(sumDeviation / numValues);
+    return sd / meanValue;
   }
 
-  private double calculateMeanIntensity(double[] intensities, int numScans) {
+  /**
+   * Calculates the mean from an array of values.
+   *
+   * @param values
+   * @param numValues
+   * @return The mean value.
+   */
+  private double calculateMeanIntensity(double[] values, int numValues) {
 
-    double sumIntensity = Arrays.stream(intensities).sum();
-    return sumIntensity / numScans;
+    double sumIntensity = Arrays.stream(values).sum();
+    return sumIntensity / numValues;
   }
 
+  /**
+   * Returns the closest features index to the given mz value. Returns -1 if the list is empty.
+   *
+   * @param mzs
+   * @return
+   */
   private int getClosestFeatureIndex(List<Double> mzs) {
 
-    if (mzs.size() == 1) {
+    //Return -1 if the list is empty.
+    if (mzs.isEmpty()) {
+      return -1;
+
+      //Return the first value if there is only one value in the list.
+    } else if (mzs.size() == 1) {
       return 0;
     }
 
     double minDifference = Double.MAX_VALUE;
     int closestIndex = 0;
 
+    //Going through the list and looking for the minimal delta mz.
     for (int i = 0; i < mzs.size(); i++) {
       double difference = Math.abs(mzs.get(i) - mz);
 
